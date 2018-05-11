e0fa2a65abeab7c05ae829930ca240e472c92976
Klhang <chrisjunhang@gmail.com>
2018-05-11 13:14:03 -0700

    create log_in page

--- web_server/client/src/App.js
+++ web_server/client/src/App.js
@@ -1,3 +1,8 @@
+import	'materialize-css/dist/css/materialize.min.css';
+import	'materialize-css/dist/js/materialize.js';
+import './App.css';
+
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';

d64ae191f77c6047579ff338194c51366f033d25
Klhang <chrisjunhang@gmail.com>
2018-05-11 13:24:51 -0700

    refactor log_in page, create log_in form

---
+++ web_server/client/src/Login/LoginPage.js
@@ -0,0 +1,97 @@
+import LoginForm from './LoginForm';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class LoginPage extends React.Component {
+  //Login Page should work with React router, so that React router can decide to show Login Page, SignUp Page or App.
+  // We use context.
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      errors: {},
+      user: {
+        email: '',
+        password: ''
+      }
+    };
+
+    this.processForm = this.processForm.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+  }
+
+  processForm(event) {
+    console.log('submit working')
+    event.preventDefault();
+
+    const email = this.state.user.email;
+    const password = this.state.user.password;
+
+    console.log('email:', email);
+    console.log('password:', password);
+
+    // Post login data
+    const url = 'http://' + window.location.hostname + ':3000/auth/login';
+    const request = new Request(
+      url,
+      {
+        method:'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      });
+
+    fetch(request).then(response => {
+      if (response.status === 200) {
+        this.setState({
+          errors: {}
+        });
+
+        response.json().then(json => {
+          console.log(json);
+
+          this.context.router.replace('/');
+        });
+      } else {
+        console.log('Login failed');
+        response.json().then(json => {
+          const errors = json.errors ? json.errors : {};
+          errors.summary = json.message;
+          this.setState({errors});
+        });
+      }
+    });
+  }
+
+  changeUser(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+
+    this.setState({
+      user
+    });
+  }
+
+  render() {
+    return (
+      <LoginForm
+        onSubmit={this.processForm}
+        onChange={this.changeUser}
+        errors={this.state.errors}
+        user={this.state.user} />
+    );
+  }
+}
+
+// To make react-router work
+LoginPage.contextTypes = {
+  router: PropTypes.object.isRequired
+};
+
+export default LoginPage;

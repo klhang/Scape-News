9281c8d95b7bbfc15d48787ac999a3fa5f69db0e
Klhang <chrisjunhang@gmail.com>
2018-05-13 13:24:26 -0700

    set up mongodb on mlab and add user modle in server

--- web_server/client/src/index.js
+++ web_server/client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from'./App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+
+//use Reactrouter to decide to show, loginPage, SingUpPage, or App, all wraped inside base
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('root'));
+
 registerServiceWorker();
